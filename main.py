6851801b0113b62b3611b205feda77edc373ccd8
AlekseyNehoroshev <legnno5@gmail.com>
2024-07-07 11:37:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import random
 client = commands.Bot(description="Ello", command_prefix=prefix)
 
  @client.command(pass_context=True)
-    async def say(ctx):
+    async def meme1(ctx):
         images = os.listdir("images")
         r_i = random.choice(images)
         with open(f"images/{r_i}","rb") as f:
            p = discord.File(f)
         await ctx.send(file=p)
 
+ @client.command(pass_context=True)
+    async def meme2(ctx):
+        images = os.listdir("images2")
+        r_i = random.choice(images)
+        with open(f"images/{r_i}","rb") as f:
+           p = discord.File(f)
+        await ctx.send(file=p)
 client.run("token")
