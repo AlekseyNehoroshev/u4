142a61d15187343e917f70c0e002f281c0108118
AlekseyNehoroshev <legnno5@gmail.com>
2024-07-07 11:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import discord
+import os
+import random
 
 client = commands.Bot(description="Ello", command_prefix=prefix)
 
  @client.command(pass_context=True)
     async def say(ctx):
-        split = client.split(" ")
-        message = str(split[2])
-        await client.say(message)
+        images = os.listdir("images")
+        r_i = random.choice(images)
+        with open(f"images/{r_i}","rb") as f:
+           p = discord.File(f)
+        await ctx.send(file=p)
 
 client.run("token")
