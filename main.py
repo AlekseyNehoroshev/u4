76e96977da128cbccaabc64819bcc980f307279b
AlekseyNehoroshev <legnno5@gmail.com>
2024-07-14 11:43:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import discord
-import os
 import random
+from discord.ext import commands
+description = 'g'
 
-client = commands.Bot(description="Ello", command_prefix=prefix)
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
 
- @client.command(pass_context=True)
-    async def meme1(ctx):
-        images = os.listdir("images")
-        r_i = random.choice(images)
-        with open(f"images/{r_i}","rb") as f:
-           p = discord.File(f)
-        await ctx.send(file=p)
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
- @client.command(pass_context=True)
-    async def meme2(ctx):
-        images = os.listdir("images2")
-        r_i = random.choice(images)
-        with open(f"images/{r_i}","rb") as f:
-           p = discord.File(f)
-        await ctx.send(file=p)
-client.run("token")
+@bot.command(name='commands')
+async def _bot(ctx):
+    await ctx.send('1.подделки из пластиклвых бутылок')
+    await ctx.send('2.самые загрязненные места(информация)')
+
+@bot.command(name='1')
+async def _bot(ctx):
+	await ctx.send('видео для идей из пластиковых бутылок: https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.youtube.com/watch%3Fv%3DRlERRK0Aea8&ved=2ahUKEwiKtMbnhaaHAxUDUqQEHcTACNMQwqsBegQIFhAF&usg=AOvVaw3g7UWQfYJiFa2tB-AP0oXt')
+
+@bot.command(name='2')
+async def _bot(ctx):
+	await ctx.send(random.chooise('https://www.google.com/amp/s/www.kommersant.ru/amp/4548601', 'https://www.rbc.ru/society/05/06/2019/5cf74b8c9a79471aab7e6281','https://www.meteovesti.ru/news/1680864930747-nazvany-goroda-rossii-s-samym-gryaznym-vozduhom'))
+	
+bot.run('token')
